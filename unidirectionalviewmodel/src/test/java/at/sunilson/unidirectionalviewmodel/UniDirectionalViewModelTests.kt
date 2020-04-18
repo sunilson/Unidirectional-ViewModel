d6bc5bcbde20e3c2c00b7c99bb1c0391c81449bc
Linus Weiss <weisslinus@gmail.com>
2020-04-18 20:17:16 +0200

    Add test stubs

---
+++ unidirectionalviewmodel/src/test/java/at/sunilson/unidirectionalviewmodel/UniDirectionalViewModelTests.kt
@@ -0,0 +1,48 @@
+package at.sunilson.unidirectionalviewmodel
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(TestCoroutineDispatcherExtension::class)
+class UniDirectionalViewModelTests {
+
+    private data class State(val string: String)
+    private sealed class Events
+    private class TestViewModel : UniDirectionalViewModel<State, Events>(State("")) {
+
+    }
+
+    private lateinit var viewModel: TestViewModel
+
+    @BeforeEach
+    fun before() {
+        viewModel = TestViewModel()
+    }
+
+    @Test
+    fun `Events are only recieved once`() {
+        TODO()
+    }
+
+    @Test
+    fun `Multiple subscribers all recieve events`() {
+        TODO()
+    }
+
+    @Test
+    fun `State is recieved upon subscription`() {
+        TODO()
+    }
+
+    @Test
+    fun `SetState and GetState are executed in correct order`() {
+        TODO()
+    }
+
+    @Test
+    fun `All middlewares are executed and in correct order`() {
+        TODO()
+    }
+
+}
