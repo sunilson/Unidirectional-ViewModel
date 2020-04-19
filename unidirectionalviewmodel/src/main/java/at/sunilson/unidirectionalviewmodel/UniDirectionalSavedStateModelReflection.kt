1f4e9ad2df2ca2ebd1aeab0a834f97b3a481e93c
Linus Weiss <weisslinus@gmail.com>
2020-04-19 14:18:37 +0200

    Extend from UniDirectionalSavedStateViewModel for reflection

--- unidirectionalviewmodel/src/main/java/at/sunilson/unidirectionalviewmodel/UniDirectionalSavedStateModelReflection.kt
+++ unidirectionalviewmodel/src/main/java/at/sunilson/unidirectionalviewmodel/UniDirectionalSavedStateModelReflection.kt
@@ -14,46 +14,42 @@ annotation class Persist
 @Target(AnnotationTarget.CLASS)
 annotation class PersistableState
 
-private fun <State : Any> State.restoreStateHandle(savedStateHandle: SavedStateHandle): State {
-    val copy = this::class.memberFunctions.firstOrNull { it.name == "copy" }
-    if (copy == null || !this::class.isData) {
-        timber.log.Timber.e("Can't restore state for non data-class!")
-        return this
-    }
-
-    val instanceParam = copy.instanceParameter
-    checkNotNull(instanceParam) { "Copy constructor instance was null!" }
-
-    val params = this::class
-        .memberProperties
-        .filter { property ->
-            property.annotations.any { it is Persist } &&
-                    savedStateHandle.get<Any>(property.name) != null
-        }
-        .associate { property ->
-            copy
-                .parameters
-                .first { it.name == property.name } to savedStateHandle.get<Any>(property.name)
-        }
-        .toMutableMap()
-
-    params[instanceParam] = this
-    return copy.callBy(params) as State
-}
-
 abstract class UniDirectionalSavedStateViewModelReflection<State : Any, Event>(
     private val initialState: State,
     private val savedStateHandle: SavedStateHandle
-) : UniDirectionalViewModel<State, Event>(initialState.restoreStateHandle(savedStateHandle)) {
+) : UniDirectionalSavedStateViewModel<State, Event>(initialState, savedStateHandle) {
 
-    init {
-        registerMiddleWare { updateStateHandle(it) }
+    override fun State.initializeStateFromSavedState(savedStateHandle: SavedStateHandle): State {
+        val copy = this::class.memberFunctions.firstOrNull { it.name == "copy" }
+        if (copy == null || !this::class.isData) {
+            timber.log.Timber.e("Can't restore state for non data-class!")
+            return this
+        }
+
+        val instanceParam = copy.instanceParameter
+        checkNotNull(instanceParam) { "Copy constructor instance was null!" }
+
+        val params = this::class
+            .memberProperties
+            .filter { property ->
+                property.annotations.any { it is Persist } &&
+                        savedStateHandle.get<Any>(property.name) != null
+            }
+            .associate { property ->
+                copy
+                    .parameters
+                    .first { it.name == property.name } to savedStateHandle.get<Any>(property.name)
+            }
+            .toMutableMap()
+
+        params[instanceParam] = this
+        return copy.callBy(params) as State
     }
 
-    private fun updateStateHandle(state: State) {
+    override fun SavedStateHandle.updateStateHandle(state: State) {
         state::class.memberProperties.forEach { property ->
             if (property.annotations.any { it is Persist }) {
-                savedStateHandle.set(
+                set(
                     property.name,
                     (property as KProperty1<State, Any>).get(state)
                 )
