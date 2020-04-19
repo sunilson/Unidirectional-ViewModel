40df006c10b17e8669e66a3d2c5d70c167783811
Linus Weiss <weisslinus@gmail.com>
2020-04-19 14:28:03 +0200

    Update assertions and error handling

--- unidirectionalviewmodel/src/main/java/at/sunilson/unidirectionalviewmodel/UniDirectionalSavedStateModelReflection.kt
+++ unidirectionalviewmodel/src/main/java/at/sunilson/unidirectionalviewmodel/UniDirectionalSavedStateModelReflection.kt
@@ -2,6 +2,7 @@ package at.sunilson.unidirectionalviewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import kotlin.reflect.KProperty1
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.instanceParameter
 import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.full.memberProperties
@@ -15,16 +16,17 @@ annotation class Persist
 annotation class PersistableState
 
 abstract class UniDirectionalSavedStateViewModelReflection<State : Any, Event>(
-    private val initialState: State,
-    private val savedStateHandle: SavedStateHandle
+    initialState: State,
+    savedStateHandle: SavedStateHandle
 ) : UniDirectionalSavedStateViewModel<State, Event>(initialState, savedStateHandle) {
 
+    @ExperimentalStdlibApi
     override fun State.initializeStateFromSavedState(savedStateHandle: SavedStateHandle): State {
+        assertPersistableState(this)
+        assertDataClass(this)
+
         val copy = this::class.memberFunctions.firstOrNull { it.name == "copy" }
-        if (copy == null || !this::class.isData) {
-            timber.log.Timber.e("Can't restore state for non data-class!")
-            return this
-        }
+        checkNotNull(copy) { "Copy constructor instance was null!" }
 
         val instanceParam = copy.instanceParameter
         checkNotNull(instanceParam) { "Copy constructor instance was null!" }
@@ -54,6 +56,23 @@ abstract class UniDirectionalSavedStateViewModelReflection<State : Any, Event>(
                     (property as KProperty1<State, Any>).get(state)
                 )
             }
+        }
+    }
+
+    private fun assertDataClass(state: State) {
+        try {
+            check(this::class.isData)
+        } catch (error: Exception) {
+            error("The state must be a data class!")
+        }
+    }
+
+    @ExperimentalStdlibApi
+    private fun assertPersistableState(state: State) {
+        try {
+            check(state::class.hasAnnotation<PersistableState>())
+        } catch (error: Exception) {
+            error("Please make sure your State class is annotated with @PersistableState")
         }
     }
 }
